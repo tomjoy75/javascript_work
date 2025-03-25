b5078988da8410491f68ec1a3fa54b114b097e94
Thomas Joyeux <tomjoy75@gmail.com>
2025-03-25 21:55:47 +0100

    prime number exercises

--- projects/javascript_info_exos/src/alert.js
+++ projects/javascript_info_exos/src/alert.js
@@ -34,7 +34,7 @@
 // }
 
 // let result = (a + b < 4) ? 'Below' : 'Over';
-// let message = (login == 'Employee') ? 'Hello' : 
+// let message = (login == 'Employee') ? 'Hello' :
 // 				(login == 'Director') ? 'Greetings':
 // 				(login == '') ? 'No login' : '';
 
@@ -72,9 +72,100 @@
 // 	answer = +prompt("Input a number", 0);
 // } while (answer < 100 && answer);
 
-// Prime numbers
-let max = +prompt("enter a number", 1);
-let result = "";
-for (let i = 2; i < max; i++){
-	
+// // Prime numbers
+// let max = +prompt("enter a number", 1);
+// if (max < 2)
+// 	alert("input too low! (>1)");
+// else {
+// 	let result = "2";
+// 	nextPrime: for (let i = 3; i < max; i++){
+// 		for (let j = 2; j < i; j++){
+// 			if ((i % j) === 0)
+// 				continue nextPrime;
+// 		}
+// 		result += ',' + i;
+// 	}
+// 	alert (result);
+// }
+
+// // Autre solution
+// function eratosthenes(n) {
+//   // Étape 1 : créer un tableau de booléens, tous initialisés à true
+//   const isPrime = new Array(n + 1).fill(true);
+//   isPrime[0] = false;
+//   isPrime[1] = false;
+
+//   // Étape 2 : éliminer les multiples
+//   for (let i = 2; i * i <= n; i++) {
+//     if (isPrime[i]) {
+//       // On élimine tous les multiples de i à partir de i*i
+//       for (let j = i * i; j <= n; j += i) {
+//         isPrime[j] = false;
+//       }
+//     }
+//   }
+//   // Étape 3 : collecter les nombres premiers
+//   const primes = [];
+//   for (let i = 2; i <= n; i++) {
+// 	  if (isPrime[i]) {
+// 		  primes.push(i);
+// 		}
+// 	}
+// 	return primes;
+// }
+// // Exemple d'utilisation :
+// const n = +prompt("enter a number", 1);
+// //const n = 100;
+// console.log(`Nombres premiers jusqu'à ${n} :`);
+// console.log(eratosthenes(n));
+
+// Rewrite
+// switch (browser) {
+//   case 'Edge':
+//     alert( "You've got the Edge!" );
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert( 'Okay we support these browsers too' );
+//     break;
+
+//   default:
+//     alert( 'We hope that this page looks ok!' );
+// }
+
+// if (browser === 'Edge')
+//     alert( "You've got the Edge!" );
+// else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera')
+//     alert( 'Okay we support these browsers too' );
+// else
+//     alert( 'We hope that this page looks ok!' );
+
+// Convert in Switch
+let a = +prompt('a?', '');
+
+// if (a == 0) {
+//   alert( 0 );
+// }
+// if (a == 1) {
+//   alert( 1 );
+// }
+
+// if (a == 2 || a == 3) {
+//   alert( '2,3' );
+// }
+
+switch (a){
+	case 0:
+		alert (0);
+		break;
+	case 1:
+		alert (1);
+		break;
+	case 2:
+	case 3:
+		alert( '2,3');
+		break;
 }
