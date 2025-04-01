95ad9c0b3381ce86f27623d851b4b4676522a4ab
Thomas Joyeux <tomjoy75@gmail.com>
2025-04-01 22:57:31 +0200

    exos JS Arrays

--- projects/javascript_info_exos/src/index_arrays.js
+++ projects/javascript_info_exos/src/index_arrays.js
@@ -24,7 +24,7 @@
 // 		if (answer === "" || answer === null || !isFinite(answer))
 // 			break;
 // 		numbers.push(+answer);
-// 	} 
+// 	}
 // 	let sum = 0;
 // 	for (let number of numbers )
 // 		sum += number;
@@ -42,7 +42,7 @@
 // 			sum += arr[j];
 // 			if (sum > max)
 // 				max = sum;
-// 		} 
+// 		}
 // 	}
 // 	return (max);
 // }
@@ -94,7 +94,7 @@
 
 // function filterRangeInPlace(arr, a, b){
 // //	arr = arr.filter(item => a <= item && b>=item)
-// 	arr.forEach((item, index, Array) => {(item < a || item > b)?Array.splice(index, 1):null}); 
+// 	arr.forEach((item, index, Array) => {(item < a || item > b)?Array.splice(index, 1):null});
 // }
 // let arr = [5, 3, 8, 1];
 // filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
@@ -106,3 +106,29 @@
 // alert( arr ); // 15, 8, 5, 2, 1, -10
 
 // Copy and sort array
+// function copySorted(arr){
+// 	return arr.slice().sort();
+// }
+// let arr = ["HTML", "JavaScript", "CSS"];
+// let sorted = copySorted(arr);
+// alert( sorted ); // CSS, HTML, JavaScript
+// alert( arr ); // HTML, JavaScript, CSS (no changes)
+
+// Extendable calculator
+function Calculator(){
+	this.calculate = function(str){
+		let operators = str.split(" ");
+		if (operators[1] === "+")
+			return +operators[0] + +operators[2];
+		else if (operators[1] === "-")
+			return +operators[0] - +operators[2];
+		else
+			alert("Wrong operator");
+	};
+	this.addMethod(name, func){
+
+	};
+}
+let calc = new Calculator;
+
+alert( calc.calculate("3 + 7") ); // 10
