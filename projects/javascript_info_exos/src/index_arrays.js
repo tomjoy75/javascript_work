8eb667d981a40ae9682dfba53bc604af0f459ab7
tomjoy75 <tomjoy75@gmail.com>
2025-04-02 15:24:13 +0200

    Exos JS

--- projects/javascript_info_exos/src/index_arrays.js
+++ projects/javascript_info_exos/src/index_arrays.js
@@ -114,33 +114,76 @@
 // alert( sorted ); // CSS, HTML, JavaScript
 // alert( arr ); // HTML, JavaScript, CSS (no changes)
 
-// Extendable calculator
-function Calculator(){
-	this.methods = [{op:"+", fn:(a, b) => a + b}, {op:"-", fn:(a,b)=>a - b}];
-	this.calculate = function(str){
-		let operators = str.split(" ");
-		let method = this.methods.find(item => item.op == operators[1]);
-		// console.log(method.fn);
-		// console.log(operators[0] + " " + operators[2]);
-		return method.fn(+operators[0], +operators[2]);
-//		console.log(operator);
-		// if (operators[1] === "+")
-		// 	return +operators[0] + +operators[2];
-		// else if (operators[1] === "-")
-		// 	return +operators[0] - +operators[2];
-		// else
-		// 	alert("Wrong operator");
-	};
-	this.addMethod = function(name, func){
-		this.methods.push({op: name, fn: func});
-//		console.log(this.methods);
-	};
+// // Extendable calculator
+// function Calculator(){
+// 	this.methods = [{op:"+", fn:(a, b) => a + b}, {op:"-", fn:(a,b)=>a - b}];
+// 	this.calculate = function(str){
+// 		let operators = str.split(" ");
+// 		let method = this.methods.find(item => item.op == operators[1]);
+// 		return method.fn(+operators[0], +operators[2]);
+// 	};
+// 	this.addMethod = function(name, func){
+// 		this.methods.push({op: name, fn: func});
+// 	};
+// }
+// let calc = new Calculator;
+// alert( calc.calculate("3 + 7") ); // 10
+// let powerCalc = new Calculator;
+// powerCalc.addMethod("*", (a, b) => a * b);
+// powerCalc.addMethod("/", (a, b) => a / b);
+// powerCalc.addMethod("**", (a, b) => a ** b);
+// let result = powerCalc.calculate("2 ** 3");
+// alert( result ); // 8
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+// let users = [ john, pete, mary ];
+// let names = users.map(item => item.name);
+// alert( names ); // John, Pete, Mary
+
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+// let users = [ john, pete, mary ];
+// //let usersMapped = users.map(({name, surname, id}) => ( { "fullName": name + " " + surname, "id" : id}));
+// let usersMapped = users.map((user) => ({
+// 	"fullName": `${user.name} ${user.surname}`,
+// 	"id": user.id,
+// }));
+// console.log(usersMapped);
+// /*
+// usersMapped = [
+//   { fullName: "John Smith", id: 1 },
+//   { fullName: "Pete Hunt", id: 2 },
+//   { fullName: "Mary Key", id: 3 }
+// ]
+// */
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // John Smith
+
+// function sortByAge(arr){
+// 	arr.sort((a , b)=> a.age - b.age);
+// }
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+// let arr = [ pete, john, mary ];
+// sortByAge(arr);
+// // now: [john, mary, pete]
+// alert(arr[0].name); // John
+// alert(arr[1].name); // Mary
+// alert(arr[2].name); // Pete
+
+// Shuffle an array
+function shuffle(arr){
+	
 }
-let calc = new Calculator;
-alert( calc.calculate("3 + 7") ); // 10
-let powerCalc = new Calculator;
-powerCalc.addMethod("*", (a, b) => a * b);
-powerCalc.addMethod("/", (a, b) => a / b);
-powerCalc.addMethod("**", (a, b) => a ** b);
-let result = powerCalc.calculate("2 ** 3");
-alert( result ); // 8
+let arr = [1, 2, 3];
+console.log(arr);
+shuffle(arr);
+console.log(arr);
+shuffle(arr);
+console.log(arr);
+shuffle(arr);
+console.log(arr);
